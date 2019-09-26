2bcdc17f07d81e12020d496debbb2841fcf1fcd5
colinj148 <colin.joyce9@gmail.com>
2019-09-26 09:52:48 -0700

    Still need to fix locationListener.

--- app/src/main/java/com/example/sensorimplementation/MainActivity.java
+++ app/src/main/java/com/example/sensorimplementation/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.sensorimplementation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init_views();
         update_time();
+        get_cords();
     }
 
     private void init_views() {
@@ -30,4 +37,22 @@ public class MainActivity extends AppCompatActivity {
         date_view.setText(time.getDate());
         time_view.setText(time.getTime());
     }
+
+    @SuppressLint("MissingPermission")
+    private void get_cords() {
+
+
+        LocationManager locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new MyLocationListerner();
+
+
+        /*TODO This is crashing app, fix*/
+
+//        locationManager.requestLocationUpdates(
+//                LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        //
+
+    }
+
 }
--- app/src/main/java/com/example/sensorimplementation/MyLocationListerner.java
+++ app/src/main/java/com/example/sensorimplementation/MyLocationListerner.java
@@ -3,12 +3,25 @@ package com.example.sensorimplementation;
 import android.location.Location;
 import android.location.LocationListener;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.content.ContentValues.TAG;
 
 
 public class MyLocationListerner implements LocationListener {
 
     @Override
     public void onLocationChanged(Location location) {
+//        Toast.makeText(
+//                getBaseContext(),
+//                "Location changed: Lat: " + location.getLatitude() + " Lng: "
+//                        + location.getLongitude(), Toast.LENGTH_SHORT).show();
+        String longitude = "Longitude: " + location.getLongitude();
+        Log.v(TAG, longitude);
+        String latitude = "Latitude: " + location.getLatitude();
+        Log.v(TAG, latitude);
 
 
     }
