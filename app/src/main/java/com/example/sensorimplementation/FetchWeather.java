564e2a39adab9ba7a41c1e2de2033ef8093c3504
colinj148 <colin.joyce9@gmail.com>
2019-10-26 17:16:22 -0700

    Adding Async to Fetch API

--- app/src/main/java/com/example/sensorimplementation/FetchWeather.java
+++ app/src/main/java/com/example/sensorimplementation/FetchWeather.java
@@ -1,59 +1,64 @@
 package com.example.sensorimplementation;
 
-import android.content.Context;
+
+import android.os.AsyncTask;
 import android.util.Log;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.RequestFuture;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONObject;
-
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 // Class for API request
-public class FetchWeather {
-
+public class FetchWeather extends AsyncTask<String, Void, String> {
+    private static String BASE_URL = "http://api.openweathermap.org/data/2.5/weather?lat=";
     private static String apiKey = "35d1d9629dd709dcbf9292faed95cc40";
     private static Double latitude = 26.614149;
     private static Double longitude = -81.825768;
-    private JSONObject jsonObject;
-    private String url;
-    private Context context;
+    private String apiResponse;
     //https://developer.android.com/training/volley/simple#java
     //Need to figure out context wrapper.
 
     public FetchWeather() {
-        context = new MyContextWrapper(this);
         Log.i("FetchWeather", "Object created");
-        buildUrl();
-        jsonObject = volleySyncRequest(context, url);
-    }
-    public String getTemp(){
-        return jsonObject.toString();
+
     }
 
-    private JSONObject volleySyncRequest(Context c, String url) {
-        RequestFuture<JSONObject> future = RequestFuture.newFuture();
-        JsonObjectRequest request = new JsonObjectRequest(url, null, future, future);
-        RequestQueue requestQueue = Volley.newRequestQueue(c);
-        requestQueue.add(request);
+    @Override
+    protected String doInBackground(String... url) {
+        HttpURLConnection con = null;
+        InputStream inputStream = null;
         try {
-            return future.get(10, TimeUnit.SECONDS);
-        } catch (InterruptedException | TimeoutException | ExecutionException e) {
+            con = (HttpURLConnection) (new URL(BASE_URL + latitude + "&lon=" + longitude + "&APPID=" + apiKey)).openConnection();
+            con.setRequestMethod("GET");
+            con.setDoInput(true);
+            con.setDoOutput(true);
+            con.connect();
+            StringBuffer buffer = new StringBuffer();
+            inputStream = con.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            String line = null;
+            while ((line = br.readLine()) != null)
+                buffer.append(line + "rn");
+
+            inputStream.close();
+            con.disconnect();
+            apiResponse = buffer.toString();
+            return apiResponse;
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            return apiResponse;
         }
-        return null;
+
     }
 
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
 
-    private void buildUrl() {
-        url = "api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon="
-                + longitude;
     }
 
 
--- app/src/main/java/com/example/sensorimplementation/MainActivity.java
+++ app/src/main/java/com/example/sensorimplementation/MainActivity.java
@@ -13,36 +13,42 @@ import android.util.Log;
 import android.widget.TextView;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 public class MainActivity extends AppCompatActivity {
     private TextView date_view;
     private TextView time_view;
     private TextView tempurature_view;
-    protected Context cxt; //passing this to FetchAPI?
 
+    @SuppressLint("WrongThread")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init_views();
         update_time();
-        //get_cords();
+        new FetchWeather().execute();
+        String output = null;
+
+        output = new FetchWeather().execute("test").get();
     }
 
-    private void init_views() {
-        date_view = findViewById(R.id.date);
-        time_view = findViewById(R.id.time);
-        tempurature_view = findViewById(R.id.temp);
-    }
+        // Log.i("api", fetchWeather.doInBackground("url"));
 
-    private void update_time() {
-        SetTime time = new SetTime();
-        date_view.setText(time.getDate());
-        time_view.setText(time.getTime());
+        private void init_views () {
+            date_view = findViewById(R.id.date);
+            time_view = findViewById(R.id.time);
+            tempurature_view = findViewById(R.id.temp);
+        }
 
-        //Log.i("api", fetch.getTemp());
-    }
+        private void update_time () {
+            SetTime time = new SetTime();
+            date_view.setText(time.getDate());
+            time_view.setText(time.getTime());
+
+            //Log.i("api", fetch.getTemp());
+        }
 
 //    private void get_cords() {
 //
@@ -95,4 +101,4 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    }
 
-}
+    }
